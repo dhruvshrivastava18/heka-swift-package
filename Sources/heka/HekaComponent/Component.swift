b9816b9579941cd353c07fb7b4f9745ebed7f560
Pulkit Goyal <7895pulkit@gmail.com>
2023-02-23 20:23:00 +0530

    remove demo struct component checked in

--- Sources/heka/HekaComponent/Component.swift
+++ Sources/heka/HekaComponent/Component.swift
@@ -1,27 +1,27 @@
-  //
-  //  Component.swift
-  //
-  //
-  //  Created by Gaurav Tiwari on 04/02/23.
-  //
+//
+//  Component.swift
+//
+//
+//  Created by Gaurav Tiwari on 04/02/23.
+//
 
 import SwiftUI
 
 public struct HekaUIView: View {
-  
+
   @ObservedObject var viewModel: ComponentViewModel
-  
+
   public init(uuid: String, apiKey: String) {
     viewModel = ComponentViewModel(uuid: uuid, apiKey: apiKey)
   }
-  
+
   public var body: some View {
     HStack {
       Image("appleHealthKit", bundle: HekaResources.resourceBundle)
         .resizable()
         .frame(width: 25, height: 25)
         .padding()
-      
+
       VStack(alignment: .leading) {
         Text("Apple HealthKit")
           .font(.headline)
@@ -31,19 +31,19 @@ public struct HekaUIView: View {
             .foregroundColor(.gray)
         }
       }
-      
+
       Spacer()
-      
+
       Button(viewModel.buttonTitle) {
         switch viewModel.currentConnectionState {
-          case .notConnected:
-            viewModel.checkHealthKitPermissions()
-            break
-          case .syncing:
-            break
-          case .connected:
-              //TODO: - Maybe add some sort of confirmation
-            viewModel.disconnectFromServer()
+        case .notConnected:
+          viewModel.checkHealthKitPermissions()
+          break
+        case .syncing:
+          break
+        case .connected:
+          //TODO: - Maybe add some sort of confirmation
+          viewModel.disconnectFromServer()
         }
       }
       .frame(width: 100, height: 40)
@@ -70,14 +70,5 @@ public struct HekaUIView: View {
     //                }
     //            )
     //        }
-  }
-}
-
-struct HekaUIView_Previews: PreviewProvider {
-  static var previews: some View {
-    HekaUIView(
-      uuid: UUID().uuidString,
-      apiKey: "7368bad8-aadd-4624-a58c-7e8af2b3cfb7"
-    )
   }
 }
