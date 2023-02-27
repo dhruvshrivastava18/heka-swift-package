be8985ccfda750e6879cde7280bd3c37252ec302
Pulkit Goyal <7895pulkit@gmail.com>
2023-02-27 18:30:32 +0530

    use self explicitly

--- Sources/heka/HekaManager.swift
+++ Sources/heka/HekaManager.swift
@@ -22,7 +22,7 @@ public class HekaManager {
     healthStore.requestAuthorization { success in
       if success {
         // TODO: we should pass in last sync date too
-        keyChainHelper.markConnected(apiKey: apiKey, uuid: userUuid)
+        self.keyChainHelper.markConnected(apiKey: apiKey, uuid: userUuid)
         completion(true)
       } else {
         completion(false)
