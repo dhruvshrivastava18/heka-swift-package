5af8c3cad6a0df13c30cee2fc33307c705eb26dd
Pulkit Goyal <7895pulkit@gmail.com>
2023-02-28 15:35:47 +0530

    fix importing of swift-log

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "heka",
-      dependencies: ["Alamofire", "PromiseKit", "Logging"],
+      dependencies: ["Alamofire", "PromiseKit", .product(name: "Logging", package: "swift-log")],
       path: "Sources",
       resources: [
         .process("Resources")
